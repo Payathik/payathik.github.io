eb5351895c541733974e689bba154949ab77ad92
Payathik <payathik06@Gmail.com>
2025-03-23 12:43:07 -0400

    Image loop
    Finished the loop for the images

--- Lab 4/Part 2/main.js
+++ Lab 4/Part 2/main.js
@@ -5,14 +5,21 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
+const imageArray = [{img: 'images/pic1.jpg'}, {img: 'images/pic2.jpg'}, {img: 'images/pic3.jpg'}, {img: 'images/pic4.jpg'}, {img: 'images/pic5.jpg'}];
 
 /* Declaring the alternative text for each image file */
-
+const text = [
+    "Interesting eye picture",
+    "Random chemical reaction",
+    "Dope flowers",
+    "Ancient Egypt stuff",
+    "Butterfly on leaf"
+];
 /* Looping through images */
-
+for (let i = 0; i < imageArray.length; i++) {
 const newImage = document.createElement('img');
 newImage.setAttribute('src', xxx);
 newImage.setAttribute('alt', xxx);
 thumbBar.appendChild(newImage);
-
+}
 /* Wiring up the Darken/Lighten button */
