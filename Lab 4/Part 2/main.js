2204b9dc5e7042f7d5c17346cdc52cd96a1ecbde
Payathik <payathik06@Gmail.com>
2025-03-28 08:49:50 -0400

    Few updates
    Just updated a few parts for part 2 and 3.

--- Lab 4/Part 2/main.js
+++ Lab 4/Part 2/main.js
@@ -5,7 +5,7 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
-const imageArray = [{img: 'images/pic1.jpg'}, {img: 'images/pic2.jpg'}, {img: 'images/pic3.jpg'}, {img: 'images/pic4.jpg'}, {img: 'images/pic5.jpg'}];
+const imageArray = ['images/pic1.jpg', 'images/pic2.jpg', 'images/pic3.jpg', 'images/pic4.jpg', 'images/pic5.jpg'];
 
 /* Declaring the alternative text for each image file */
 const text = [
@@ -17,15 +17,27 @@ const text = [
 ];
 /* Looping through images */
 for (let i = 0; i < imageArray.length; i++) {
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
-thumbBar.appendChild(newImage);
+  const newImage = document.createElement('img');
+  newImage.setAttribute('src', imageArray[i]);
+  newImage.setAttribute('alt', text[i]);
+  thumbBar.appendChild(newImage);
+  newImage.addEventListener('click', () => {
+    displayedImage.src = newImage.src;
+    displayedImage.alt = newImage.alt;
+});
 }
 
 // click
-newImage.addEventListener('click', () => {
-    displayedImage.src = newImage.src;
-    displayedImage.alt = newImage.alt;
-  });
+
 /* Wiring up the Darken/Lighten button */
+btn.addEventListener('click', () => {
+  if (btn.getAttribute('class') === 'dark') {
+      btn.setAttribute('class', 'light');
+      btn.textContent = 'Lighten';
+      overlay.style.backgroundColor = 'rgb(0 0 0 / 50%)';
+  } else {
+      btn.setAttribute('class', 'dark');
+      btn.textContent = 'Darken';
+      overlay.style.backgroundColor = 'rgb(0 0 0 / 0%)';
+  }
+});
