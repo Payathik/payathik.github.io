e42daf2f8063ae70527558264ca3f9c21b76fd63
Payathik <payathik06@Gmail.com>
2025-04-04 22:09:35 -0400

    Commit 1 Part 4
    Completed a few parts, gonna continue the rest of the features

--- Lab 4/Part 4/main-finished.js
+++ Lab 4/Part 4/main-finished.js
@@ -18,15 +18,24 @@ function randomRGB() {
   return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
 }
 
-class Ball {
+
+class Shape {
   constructor(x, y, velX, velY, color, size) {
     this.x = x;
     this.y = y;
     this.velX = velX;
     this.velY = velY;
+
+  } }
+
+class Ball extends Shape {
+  constructor(x, y, velX, velY, color, size ,  exists) {
+    super(x, y, velX, velY);
     this.color = color;
     this.size = size;
+    this.exists = true;
   }
+
 
   draw() {
     ctx.beginPath();
@@ -58,18 +67,18 @@ class Ball {
 
   collisionDetect() {
     for (const ball of balls) {
-      if (!(this === ball)) {
+      if (!(this === ball) && ball.exists) {
         const dx = this.x - ball.x;
         const dy = this.y - ball.y;
         const distance = Math.sqrt(dx * dx + dy * dy);
-
+  
         if (distance < this.size + ball.size) {
           ball.color = this.color = randomRGB();
         }
       }
     }
   }
-}
+}  
 
 const balls = [];
 
