7de0fa11526df58389956dc7e4ccbce4abb89061
Payathik <payathik06@Gmail.com>
2025-04-04 22:14:58 -0400

    Two method added

--- Lab 4/Part 4/main-finished.js
+++ Lab 4/Part 4/main-finished.js
@@ -111,7 +111,44 @@ class EvilCircle extends Shape {
       ctx.stroke();
       ctx.lineWidth = 3
     }    
+    // checkbound method
+    checkBounds() {
+    
+      if (this.x + this.size >= width) {
+        this.x = width - this.size; 
+      }
+    
+      if (this.x - this.size <= 0) {
+        this.x = this.size; 
+      }
+    
+      if (this.y + this.size >= height) {
+        this.y = height - this.size; 
+      }
+    
+      if (this.y - this.size <= 0) {
+        this.y = this.size; 
+      }
+    }
 
+    // Collisiondetect method 
+    collisionDetect() {
+      for (const ball of balls) {
+        if (ball.exists) {
+          const dx = this.x - ball.x;
+          const dy = this.y - ball.y;
+          const distance = Math.sqrt(dx * dx + dy * dy);
+    
+          if (distance < this.size + ball.size) {
+            ball.exists = false;
+            count--
+            updateBallCount()
+          }
+        }
+      }
+    }
+  }
+}
 
 const balls = [];
 
